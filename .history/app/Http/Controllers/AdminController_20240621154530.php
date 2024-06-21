6192b11265fbea49d82c6449dc050958ec1d2fae
SayedAbdo2 <sayed220@gmail.com>
2024-06-21 18:10:28 +0300

    show_posts_and_delete

---
+++ .history/app/Http/Controllers/AdminController_20240621154530.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AdminController extends Controller
+{
+    public function post_page(){
+        return view('admin.posts.post_page');
+    }
+    public function add_post(Request $request){
+
+        //users attributes
+        $user = Auth()->user();
+        $userId = $user->id;
+        $name = $user->name;
+        $usertype = $user->usertype;
+
+        $post = new Post;
+        $post->title = $request->title;
+        $post->description = $request->description;
+
+        $image = $request->image;
+        if($image){ //---Error 1
+            $imageName = time() . '.' . $image->getClientOriginalExtension();
+            $image->move('postImages', $imageName);
+            $post->image = $imageName;
+        }
+
+        $post->post_status = 'active';
+        $post->user_id = $userId;
+        $post->name = $name;
+        $post->usertype = $usertype;
+        $post->save();
+
+        return redirect()->back()->with('message', 'Post Add Successfully!');
+    }
+
+    public function show_posts(){
+        return view('admin.po')
+    }
+}
---
+++ .history/resources/views/admin/posts/show_posts.blade_20240621164440.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+
+<html lang="en" class="light-style layout-menu-fixed" dir="ltr" data-theme="theme-default" data-assets-path="../assets/"
+    data-template="vertical-menu-template-free">
+
+<head>
+    @include('admin.head')
+</head>
+
+<body>
+    <!-- Layout wrapper -->
+    <div class="layout-wrapper layout-content-navbar">
+        <div class="layout-container">
+            <!-- Menu -->
+
+            @include('admin.asideMenu')
+            <!-- / Menu -->
+
+            <!-- Layout container -->
+            <div class="layout-page">
+                <!-- Navbar -->
+                @include('admin.navbar')
+
+                <!-- / Navbar -->
+
+                <!-- Content wrapper -->
+                <div class="content-wrapper">
+                    <!-- Content -->
+
+                    <h1 class="text-center text-uppercase mt-5">All Posts</h1>
+
+                    <div class="row mx-3">
+
+                        <div class="card">
+                            <h5 class="card-header">All Posts</h5>
+                            <div class="table-responsive text-nowrap">
+                                <table class="table">
+                                    <thead>
+                                        <tr>
+                                            <th>Title</th>
+                                            <th>Description</th>
+                                            <th>Users</th>
+                                            <th>Status</th>
+                                            <th>Images</th>
+                                            <th>Actions</th>
+                                        </tr>
+                                    </thead>
+                                    
+                                    @foreach ($posts as $post)
+                                    <tbody class="table-border-bottom-0">
+                                        <tr>
+                                            <td>
+                                                <strong>{{$post->title}}</strong>
+                                            </td>
+                                            <td><span class="d-block">{{$post->description}}</span></td>
+                                            <td>
+                                                {{$post->name}}
+                                            </td>
+                                            <td><span class="badge bg-label-primary me-1">{{$post->post_status}}</span></td>
+                                            <td>
+                                                <img src="postImages/{{$post->image}}" style="width: 80px;"  alt="">
+                                            </td>
+                                            <td class="d-flex justify-content-center">
+                                                <a class="" href="#">
+                                                    <i class="bx bx-edit-alt me-1"></i>
+                                                </a>
+                                                <a class="mx-2 text-danger" href="#">
+                                                    <i class="bx bx-trash me-1 "></i>
+                                                </a>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                    @endforeach
+                                </table>
+                            </div>
+                            <!-- Overlay -->
+                            <div class="layout-overlay layout-menu-toggle"></div>
+                        </div>
+                    </div>
+
+                    <!-- Overlay -->
+                    <div class="layout-overlay layout-menu-toggle"></div>
+                </div>
+            </div>
+        </div>
+        <!-- / Layout wrapper -->
+
+
+
+        <!-- Core JS -->
+        <!-- build:js assets/vendor/js/core.js -->
+        @include('admin.scripts')
+</body>
+
+</html>
