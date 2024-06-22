49e63da8818e2b0e37270d08c98721dc9d526073
SayedAbdo2 <sayed220@gmail.com>
2024-06-22 18:52:33 +0300

    show_posts_of_user_and_delete

---
+++ .history/resources/views/layouts/Front/user/user_allPosts.blade_20240622154231.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    @include('layouts.Front.head')
+</head>
+
+<body class="index-page">
+
+    {{--Header --}}
+    @include('layouts.Front.header')
+    @include('layouts.Front.singlePage')
+
+    <section id="#" class="services section">
+        <!-- Section Title -->
+        <div class="container section-title" data-aos="fade-up">
+            <h2>Create New Post</h2>
+            <p>Read about the most important programming and web development topics</p>
+        </div><!-- End Section Title -->
+
+        <div class="container">
+            <div class="row gy-4">
+
+                <!-- Basic Layout -->
+                <div class="card">
+                    <div class="card-body">
+                        All Posts of user
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    {{--footer--}}
+    @include('layouts.Front.footer')
+    <!-- Scroll Top -->
+    <a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center"><i
+            class="bi bi-arrow-up-short"></i></a>
+
+    <!-- Preloader -->
+    <div id="preloader"></div>
+
+    {{--javascribt--}}
+    @include('layouts.Front.script')
+
+</body>
+
+</html>
---
+++ .history/resources/views/layouts/Front/user/user_allPosts.blade_20240622154537.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    @include('layouts.Front.head')
+</head>
+
+<body class="index-page">
+
+    {{--Header --}}
+    @include('layouts.Front.header')
+    @include('layouts.Front.singlePage')
+
+    <section id="#" class="services section">
+        <!-- Section Title -->
+        <div class="container section-title" data-aos="fade-up">
+            <h2>Create New Post</h2>
+            <p>Read about the most important programming and web development topics</p>
+        </div><!-- End Section Title -->
+
+        <div class="container">
+            <div class="row gy-4">
+
+                @if($posts->isEmpty())
+                <p>No posts available.</p>
+                @endif
+
+                @foreach ($posts as $post )
+
+                <div class="col-xl-3 col-md-6 d-flex" data-aos="fade-up" data-aos-delay="100">
+                    <div class="card">
+                        <img class="card-img-top" src="postImages/{{$post->image}}" alt="Card image cap" />
+                        <div class="card-body">
+                            <h5 class="card-title">{{$post->title}}</h5>
+                        </div>
+                        <div class="card-footer">
+                            <small class="text-muted">Post by : {{$post->name}}</small>
+                        </div>
+                        <div class="card-footer">
+                            <a href="#">Show</a>
+                        </div>
+                    </div>
+                </div><!-- End Service Item -->
+                @endforeach
+            </div>
+        </div>
+    </section>
+    {{--footer--}}
+    @include('layouts.Front.footer')
+    <!-- Scroll Top -->
+    <a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center"><i
+            class="bi bi-arrow-up-short"></i></a>
+
+    <!-- Preloader -->
+    <div id="preloader"></div>
+
+    {{--javascribt--}}
+    @include('layouts.Front.script')
+
+</body>
+
+</html>
