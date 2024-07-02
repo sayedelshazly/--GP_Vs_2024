619a909e9abf8161694cc0c6e46dffe270b410f9
SayedAbdo2 <sayed220@gmail.com>
2024-07-03 01:00:37 +0300

    some updates

---
+++ .history/resources/views/layouts/Front/hero.blade_20240703002346.php
@@ -0,0 +1,34 @@
+<section id="hero" class="hero section" style="background-image: url('/assets/img/bg-hero.png'); back">
+
+    <div class="container">
+        <div class="col-lg-3 order-1 order-lg-2 hero-img" data-aos="zoom-out" data-aos-delay="200">
+            {{-- <img src="{{asset('postImages/hero-img.png')}}" class="img-fluid w-25 animated" alt=""> --}}
+        </div>
+        <div class="row gy-4">
+            <div class="w-75 mx-auto text-center d-flex flex-column justify-content-center" data-aos="zoom-out">
+                <p class="fs-1 text-light">Learn Programming Topics and Web development <i class="bi bi-emoji-smile text-info"></i></p>
+                <p class="">We best choice to work as a software engineer and growth you knowledge</p>
+                <div class="d-flex justify-content-center">
+                    <a href="#about" class="btn-get-started">Get Started</a>
+                    <a href="https://www.youtube.com/watch?v=JwCeoLGg6-U"
+                        class="glightbox btn-watch-video d-flex align-items-center"><i
+                            class="bi bi-play-circle"></i><span>Watch Video</span></a>
+                </div>
+            </div>     
+        </div>
+    </div>
+    
+    <ul class="mx-5 list-unstyled badge text-bg-info bottom-0 mb-5 d-flex mt-5 text-light rounded p-2 position-fixed" style="font-size: 15px; background-color:#37517ebb !important; z-index:1000; margin-bottom:175px !important">
+        {{-- <li class="mx-2 text-uppercase "><a href="#hero">Home</a></li> --}}
+        <li class="mx-2 text-uppercase "><a class="text-light" href="#about">About</a></li>
+        <li class="mx-2 text-uppercase "><a class="text-light" href="#posts">Posts</a></li>
+        <li class="mx-2 text-uppercase "><a class="text-light"href="#products">Products</a></li>
+        <li class="mx-2 text-uppercase "><a class="text-light"href="#services">services</a></li>
+        <li class="mx-2 text-uppercase "><a class="text-light"href="#team">Team</a></li>
+        <li class="mx-2 text-uppercase "><a class="text-light"href="#pricing">Pricing</a></li>
+        <li class="mx-2 text-uppercase "><a class="text-light"href="#contact">Contact</a></li>
+    </ul>
+    
+    
+
+</section><!-- /Hero Section -->
